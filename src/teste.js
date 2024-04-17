84477d19f8120d8a0fbc021e8fa783bfc7bd475a
Thiago Carvalho <thiago.carvalho1991@hotmail.com>
2024-04-17 07:48:44 -0300

    Primerio Commit

---
+++ src/teste.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplicação(numero1, numero2) {
+    const resultado = numero1 * numero2;
+    console.log(`A multiplicação dos argumentos é ${resultado}`);
+}
+function saudação(nome) {
+    const saudação = `Olá ${nome}`;
+    console.log(saudação);
+}
+multiplicação(11, 10);
+saudação('Thiago Carvalho');
---
+++ src/teste.ts
@@ -0,0 +1,19 @@
+
+
+
+function multiplicação(numero1: number, numero2: number): void{
+
+    const resultado = numero1 * numero2;
+    console.log(`A multiplicação dos argumentos é ${resultado}`);
+}
+
+function saudação(nome: string): void{
+    const saudação = `Olá ${nome}`;
+    console.log(saudação);
+}
+
+
+
+multiplicação(11, 10);
+saudação('Thiago Carvalho');
+
